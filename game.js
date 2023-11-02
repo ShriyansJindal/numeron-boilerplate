731ba8b6b41e496aa56d98b1a180a767f9ecc87d
shriyans jindal <shriyansjindal40517@gmail.com>
2023-11-02 17:13:41 +0530

    done

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+const n1 = document.getElementById("number1")
+const n2 = document.getElementById("number2")
+const greater = document.getElementById("greater-than")
+const equal = document.getElementById("equal-to")
+const lesser = document.getElementById("lesser-than")
+const number1 = document.getElementById("number1")
+const number2 = document.getElementById("number2")
 // Iteration 2: Generate 2 random number and display it on the screen
 
 // Iteration 3: Make the options button functional
